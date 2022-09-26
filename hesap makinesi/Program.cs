6dea67ddd041044b06dd345d663d36cc8fa52a66
yavru <yavru@DESKTOP-02O23ES>
2022-09-26 15:01:24 +0300

    Add project files.

---
+++ hesap makinesi/Program.cs
@@ -0,0 +1,39 @@
+﻿
+double sayi1, sayi2, hesaplama;
+
+Console.WriteLine("lütfen ilk sayıyı giriniz ");
+sayi1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("lütfen ikinci sayıyi giriniz ");
+sayi2 =Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("lütfen yapılacak işlemi seçiniz ");
+
+string islem = Console.ReadLine();
+
+
+if (islem == "+")
+{
+    hesaplama = sayi1 + sayi2;
+ 
+    Console.WriteLine("  sonuç= " + hesaplama);
+}
+ else if (islem == "-")
+{
+    hesaplama = sayi1 - sayi2;
+    Console.WriteLine("  sonuç= " + hesaplama);
+}
+ else if (islem == "/")
+{
+    hesaplama = sayi1 / sayi2;
+    Console.WriteLine("  sonuç= " + hesaplama);
+}
+ else if (islem == "*")
+{
+    hesaplama = sayi1 * sayi2;
+    Console.WriteLine("  sonuç= " + hesaplama);
+}
+else
+{
+    Console.WriteLine("yanlış işlm girdiniz lütfen tekrar deneyiniz");
+
+}
+Console.Read();
